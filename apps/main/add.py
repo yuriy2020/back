1bd450cb530fde9aac8026552402db86510eabc9
RMP\V.Komarov <V.Komarov@rosmorport.ru>
2024-05-20 12:00:46 +0300

    fix2

--- apps/main/add.py
+++
@@ -1,7 +0,0 @@
-from apps.choices import COUNTRIES
-from apps.models import CountryModel
-
-
-for country in COUNTRIES:
-    country = CountryModel(label=country[0], phone=country[1], code=country[2])
-    country.save()
--- apps/main/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppConf(AppConfig):
-    name = 'apps'
-    ververbose_name = 'App'
